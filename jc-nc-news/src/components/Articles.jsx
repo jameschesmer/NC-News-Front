25cdaf4db8a40439b442b89b6689c87794d55a6d
jameschesmer <jameschesmer@gmail.com>
2018-09-19 10:28:58 +0100

    set up components

---
+++ jc-nc-news/src/components/Articles.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Articles extends Component {
+  state = {
+    articles: []
+  }
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+export default Articles;
